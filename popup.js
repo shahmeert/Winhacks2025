f192cc5d254d6291ad92d0b4dea2066c76835286
Shahmeert <shahmeer511@gmail.com>
2025-02-15 16:27:43 -0500

    changed button functionality

--- popup.js
+++ popup.js
@@ -2,18 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const startTracker = document.getElementById("startTracker");
     if (startTracker) {
         startTracker.addEventListener("change", (event) => {
-            if (event.target.checked) {
-                alert("we are LIVE!");
+            if (event.target.checked) { 
                 document.body.style.transition = "background-color 1s";
                 document.body.style.backgroundColor = "#000000"; 
-
                 document.body.style.width = "300px";
                 document.body.style.height = "300px";
             } else {
                 alert("Tracking stopped!");
                 document.body.style.backgroundColor = "";
-                document.body.style.width = "300";
-                document.body.style.height = "300";
+                document.body.style.width = "300px";
+                document.body.style.height = "300px";
             }
         });
     }
