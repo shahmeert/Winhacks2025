be62cf37a2c77c95b2310547a66c56d0814c5513
ab15hek <121849673+ab15hek@users.noreply.github.com>
2025-02-15 16:43:52 -0500

    Css put the button to the top right

--- popup.js
+++ popup.js
@@ -3,17 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     if (startTracker) {
         startTracker.addEventListener("change", (event) => {
             if (event.target.checked) {
+                alert("we are LIVE!");
                 document.body.style.transition = "background-color 1s";
                 document.body.style.backgroundColor = "#000000"; 
-                text.style.color = "red";
 
                 document.body.style.width = "300px";
                 document.body.style.height = "300px";
             } else {
                 alert("Tracking stopped!");
                 document.body.style.backgroundColor = "";
-                document.body.style.width = "300px";
-                document.body.style.height = "300px";
+                document.body.style.width = "300";
+                document.body.style.height = "300";
             }
         });
     }
