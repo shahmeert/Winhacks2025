2191054e2f9f5aa36543b6e6a585b14fb5fcdab1
DeclanMitchell90 <declanmitchell90@gmail.com>
2025-02-15 17:11:21 -0500

    Added gif

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 const forbiddenUrls = [
     "https://www.netflix.*/*", "https://www.primevideo.*/*", "https://www.youtube.*/*", "https://www.hulu.*/*",
     "https://www.disneyplus.*/*", "https://www.crunchyroll.*/*", "https://www.twitch.*/*", "https://www.facebook.*/*",
-    "https://www.instagram.*/*", "https://www.snapchat.*/*", "https://www.twitter.*/*", "https://www.tiktok.*/*",
+    "https://www.instagram.*/*", "https://www.snapchat.*/*", "https://www.x.*/*", "https://www.tiktok.*/*",
     "https://www.pinterest.*/*", "https://www.reddit.*/*", "https://www.whatsapp.*/*", "https://www.messenger.*/*",
     "https://web.whatsapp.*/*", "https://www.discord.*/*", "https://www.skype.*/*", "https://www.steampowered.*/*",
     "https://www.epicgames.*/*", "https://www.roblox.*/*",  "https://www.reddit.*/*",
