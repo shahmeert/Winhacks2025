ab84726a7616aaa02d86a0948da5c6a2f2556fa4
DeclanMitchell90 <declanmitchell90@gmail.com>
2025-02-15 14:47:41 -0500

    Added gif

--- popup.js
+++ popup.js
@@ -2,8 +2,6 @@ document.getElementById("myButton").addEventListener("click", () => {
     alert("we are LIVE!");
     document.body.style.transition = "background-color 1s";
     document.body.style.backgroundColor = "#add8e6"; // light blue
-    const id = document.getElementById("SIR");
-    gif.style.display = "block";
     });
 document.body.style.width = "300px";
 document.body.style.height = "300px";
