2208ffa2d5b8f8a8b58cfeb06615968e4a2321ac
DeclanMitchell90 <declanmitchell90@gmail.com>
2025-02-15 19:08:26 -0500

    Added gif

--- popup.js
+++ popup.js
@@ -1,33 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const startTracker = document.getElementById("startTracker");
-    
-
-
-    chrome.storage.local.get(["trackerEnabled"], (result) => {
-        if (result.trackerEnabled) {
-            startTracker.checked = true;
-            document.body.style.backgroundColor = "#000000"; 
-        }
-    });
-
     if (startTracker) {
         startTracker.addEventListener("change", (event) => {
             if (event.target.checked) { 
                 document.body.style.transition = "background-color 1s";
                 document.body.style.backgroundColor = "#000000"; 
-                
-                centeredText.style.color = "#ffffff";
-                cen2.style.color = "#ffffff";
-                
                 document.body.style.width = "300px";
                 document.body.style.height = "300px";
             } else {
                 alert("Tracking stopped!");
                 document.body.style.backgroundColor = "";
-
-                if (typeof centeredText !== 'undefined' && centeredText) centeredText.style.color = ""; 
-                if (typeof cen2 !== 'undefined' && cen2) cen2.style.color = "";
-                
                 document.body.style.width = "300px";
                 document.body.style.height = "300px";
             }
