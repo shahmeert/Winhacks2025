9deb6acc99ed9c4f557fbe14f423fa4f7293d921
Shahmeert <shahmeer511@gmail.com>
2025-02-16 09:51:01 -0500

    debugging

--- background.js
+++ background.js
@@ -7,19 +7,9 @@ const forbiddenUrls = [
     "https://www.epicgames.*/*", "https://www.roblox.*/*",  "https://www.reddit.*/*",
     "https://www.nytimes.*/*"
 ];
-
-const forbiddenUrls = [
-    "https://www.netflix.", "https://www.primevideo.", "https://www.youtube.", "https://www.hulu.",
-    "https://www.disneyplus.", "https://www.crunchyroll.", "https://www.twitch.", "https://www.facebook.",
-    "https://www.instagram.", "https://www.snapchat.", "https://www.twitter.", "https://www.tiktok.",
-    "https://www.pinterest.", "https://www.reddit.", "https://www.whatsapp.", "https://www.messenger.",
-    "https://web.whatsapp.", "https://www.discord.", "https://www.skype.", "https://www.steampowered.",
-    "https://www.epicgames.", "https://www.roblox.", "https://www.nytimes."
-];
-
 function checkForbiddenTabs() {
     chrome.storage.local.get(["focusMode"], (data) => {
-        if (data.focusMode) { // Only block if focus mode is enabled
+        if (data.focusMode) { 
             chrome.tabs.query({}, (tabs) => {
                 tabs.forEach((tab) => {
                     forbiddenUrls.some((urlPattern) => {
@@ -33,7 +23,7 @@ function checkForbiddenTabs() {
                             });
 
                             chrome.tabs.update(tab.id, { url: "focus.html" });
-                            return true; // Exit loop early if match found
+                            return true;
                         }
                         return false;
                     });
